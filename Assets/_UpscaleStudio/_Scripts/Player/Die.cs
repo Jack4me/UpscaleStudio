69d29802759e55ab52882817837fa1c3295924c5
Jack4me <just4go@gmail.com>
2024-09-20 21:37:35 +0200

    +10

--- Assets/_UpscaleStudio/_Scripts/Player/Die.cs
+++ Assets/_UpscaleStudio/_Scripts/Player/Die.cs
@@ -1,3 +1,4 @@
+using _UpscaleStudio._Scripts.Data.SoundData;
 using _UpscaleStudio._Scripts.Screens;
 using _UpscaleStudio._Scripts.System.Handlers;
 using UnityEngine;
@@ -5,12 +6,14 @@ using UnityEngine;
 namespace _UpscaleStudio._Scripts.Player {
     public class Die : MonoBehaviour {
         [SerializeField] private PlayerHandle player;
+        [SerializeField] private SoundData dyingSound;
 
         public void HandleDeath() {
             
-           
+
             SoundHandler.Instance.StopAllSounds();
             player.playerCamera.DisableRotation();
+            SoundHandler.Instance.PlaySound(dyingSound, transform.position, gameObject);
             Time.timeScale = 0f;
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
